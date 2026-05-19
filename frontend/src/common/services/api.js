0d24aa61b9f39ef2ce952ffe7a2e65ab6da8e658
Balamurugan <balamurugannallathambi2005@gmail.com>
2026-05-19 21:17:34 +0530

    fixed deployed api url

--- frontend/src/common/services/api.js
+++ frontend/src/common/services/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
-
+const API_BASE_URL = 'https://learning-web-platform-and-advanced-quiz.onrender.com/api';
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
